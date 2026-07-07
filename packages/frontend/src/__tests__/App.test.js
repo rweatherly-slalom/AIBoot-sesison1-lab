add17f5ce686b68d346511eeb31182f2e93ffa2c
rweatherly-slalom <rob.weatherly@slalom.com>
2026-07-07 18:08:36 +0000

    Update header text in App component and corresponding test

--- packages/frontend/src/__tests__/App.test.js
+++ packages/frontend/src/__tests__/App.test.js
@@ -50,7 +50,7 @@ describe('App Component', () => {
     await act(async () => {
       render(<App />);
     });
-    expect(screen.getByText('React Frontend with Node Backend')).toBeInTheDocument();
+    expect(screen.getByText('hellow world 🧚🚀')).toBeInTheDocument();
     expect(screen.getByText('Connected to in-memory database')).toBeInTheDocument();
   });
 
